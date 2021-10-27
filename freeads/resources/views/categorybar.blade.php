9f3e2d503166aefab71e6d69c5c88172422dd09f
Florian <florian.barbereau@epitech.eu>
2021-10-27 15:55:14 +0200

    Update.

--- freeads/resources/views/categorybar.blade.php
+++ freeads/resources/views/categorybar.blade.php
@@ -1,52 +1,32 @@
 <div class="bg-light border-bottom">
-    <div class="container d-flex flex-row flex-wrap categories">
-        <div class="dropdown flex-fill text-center category">
-            <a href="#" class="btn dropdown-toggle" data-bs-toggle="dropdown" id="dropdownMenuButton1">
-                    Cars & Vehicles
-            </a>
-            <ul class="dropdown-menu" aria-labelledby="dropdownMenuButton1">
-                <li><a class="dropdown-item" href="#">Action</a></li>
-                <li><a class="dropdown-item" href="#">Another action</a></li>
-                <li><a class="dropdown-item" href="#">Something else here</a></li>
-            </ul>
+    <div class="accordion" id="accordionExample">
+        <div class="container d-flex flex-row flex-wrap categories">
+            <i class="align-self-center sbi bi-three-dots-vertical"></i>
+            @foreach($categories as $rootCategory)
+                @if($rootCategory->parent_id == NULL)
+                <div class="flex-fill text-center category">
+                    <a class="btn" type="button" data-bs-toggle="collapse" data-bs-target="#collapse{{$rootCategory->id}}" aria-expanded="true" aria-controls="collapse{{$rootCategory->id}}">
+                        {{ $rootCategory->name }}
+                    </a>
+                </div>
+                <i class="align-self-center sbi bi-three-dots-vertical"></i>
+                @endif
+            @endforeach
         </div>
-        <i class="align-self-center sbi bi-three-dots-vertical"></i>
-        <div class="dropdown flex-fill text-center category">
-            <a href="#" class="btn dropdown-toggle" data-bs-toggle="dropdown" id="dropdownMenuButton2">
-                    For Sale
-            </a>
-            <ul class="dropdown-menu" aria-labelledby="dropdownMenuButton2">
-                <li><a class="dropdown-item" href="#">Action</a></li>
-                <li><a class="dropdown-item" href="#">Another action</a></li>
-                <li><a class="dropdown-item" href="#">Something else here</a></li>
-            </ul>
-        </div>
-        <i class="align-self-center sbi bi-three-dots-vertical"></i>
-        <div class="dropdown flex-fill text-center category">
-            <a href="#" class="btn dropdown-toggle" data-bs-toggle="dropdown" id="dropdownMenuButton3">
-                    Property
-            </a>
-            <ul class="dropdown-menu" aria-labelledby="dropdownMenuButton3">
-                <li><a class="dropdown-item" href="#">Action</a></li>
-                <li><a class="dropdown-item" href="#">Another action</a></li>
-                <li><a class="dropdown-item" href="#">Something else here</a></li>
-            </ul>
-        </div>
-        <i class="align-self-center sbi bi-three-dots-vertical"></i>
-        <div class="flex-fill text-center category">
-            <a href="#" class="btn">Jobs</a>
-        </div>
-        <i class="align-self-center sbi bi-three-dots-vertical"></i>
-        <div class="flex-fill text-center category">
-            <a href="#" class="btn">Services</a>
-        </div>
-        <i class="align-self-center sbi bi-three-dots-vertical"></i>
-        <div class="flex-fill text-center category">
-            <a href="#" class="btn">Community</a>
-        </div>
-        <i class="align-self-center sbi bi-three-dots-vertical"></i>
-        <div class="flex-fill text-center category">
-            <a href="#" class="btn">Pets</a>
-        </div>
+        @foreach($categories as $rootCategory)
+            @if($rootCategory->parent_id == NULL)
+                <div id="collapse{{$rootCategory->id}}" class="accordion-collapse collapse container p-3" aria-labelledby="heading{{$rootCategory->id}}" data-bs-parent="#accordionExample">
+                    <div class="row">
+                    @foreach($categories as $subCategory)
+                        @if($subCategory->parent_id == $rootCategory->id)
+                            <div class="col-lg-4">
+                                <a href="/category/{{$subCategory->id}}" class="btn">{{$subCategory->name}}</a>
+                            </div>
+                        @endif
+                    @endforeach
+                    </div>
+                </div>
+            @endif
+        @endforeach
     </div>
 </div>
