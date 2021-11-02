48dcfaefa186cd4198230a05c000ff427e002647
SIM Chia Huey <sim.chiahuey@gmail.com>
2021-11-02 12:06:56 +0100

    Add example-app

---
+++ freeads/resources/views/header.blade.php
@@ -0,0 +1,28 @@
+<div class="bg-steelblue">
+    <div class="container d-flex flex-row align-items-center p-2">
+        <div class="me-auto">
+            <a href="/" class="btn">
+                <h1><span class='text-light'>Free</span>
+                <span class="ms-2 text-duckblue"><i class="align-self-center bi bi-badge-ad-fill"></i></span></h1>
+            </a>
+        </div>
+        <div class="d-flex flex-column">
+            <a href="#" class="btn btn-md btn-outline-light mx-1 border-0">
+                <div class="text-center"><i class="bi bi-pin-angle"></i></div>
+                <div class="text-center">Post ad</div>
+            </a>
+        </div>
+        <div class="d-flex flex-column">
+            <a href="#" class="btn btn-md btn-outline-light mx-1 border-0">
+                <div class="text-center"><i class="bi bi-binoculars"></i></div>
+                <div class="text-center">Search</div>
+            </a>
+        </div>
+        <div class="d-flex flex-column">
+            <a href="#" class="btn btn-md btn-outline-light mx-1 border-0">
+                <div class="text-center"><i class="bi bi-box-arrow-in-right"></i></div>
+                <div class="text-center">Login/Register</div>
+            </a>
+        </div>
+    </div>
+</div>
