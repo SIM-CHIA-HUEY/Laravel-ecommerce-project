ba45fc5a120cf1720cf1b8663a53c2a6aa9a4fa5
SIM Chia Huey <sim.chiahuey@gmail.com>
2021-11-02 14:08:43 +0100

    Change folder name to freeads

---
+++ freeads/resources/views/searchbar.blade.php
@@ -0,0 +1,10 @@
+<div  class="bg-lightblue">
+    <div class="">
+        <form method="post" action="{{url('/search')}}" class="container d-flex flex-row align-items-center p-2">
+            @csrf
+            <input type="text" class="form-control mx-2" name="search" placeholder="I'm looking for ..."/> 
+            in <input type="text" class="form-control mx-2" name="location" placeholder="Postcode or location"/>
+            <button type="submit" class="btn btn-danger mx-2">GO</button>
+        </form>
+    </div>
+</div>
